91cf573452fb5dc7f93c00bff898d7b5b4d5e4d2
David de Tena <daviddetenacalvo@gmail.com>
2015-04-24 10:09:16 +0200

    Initial Commit

---
+++ HackerBooks_v2Tests/HackerBooks_v2Tests.m
@@ -0,0 +1,40 @@
+//
+//  HackerBooks_v2Tests.m
+//  HackerBooks_v2Tests
+//
+//  Created by David de Tena on 24/04/15.
+//  Copyright (c) 2015 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HackerBooks_v2Tests : XCTestCase
+
+@end
+
+@implementation HackerBooks_v2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
